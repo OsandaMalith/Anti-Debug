28b25449492e1c528638790435f6333cddcff4a3
Osanda Malith Jayathissa <osandajayathissa@gmail.com>
2016-05-28 07:00:21 -0700

    Update ppid.asm

--- PPID/ppid.asm
+++ PPID/ppid.asm
@@ -15,20 +15,20 @@ includelib user32.lib
 includelib kernel32.lib
 
 .data
-exp			db 	"explorer.exe",0
+exp		db 	"explorer.exe",0
 AppName		db 	"Status",0
 errSnapshot    	db 	"CreateToolhelp32Snapshot failed.",0
-errProcFirst    	db 	"Process32First failed.",0
+errProcFirst    db 	"Process32First failed.",0
 errExolorer 	db 	"Explorer.exe Not Found!"
-expfound 		db 	"Explorer.exe Found",0
+expfound 	db 	"Explorer.exe Found",0
 dbgFound 	db 	"Debugger Found!", 0
 dbgNotFound	db 	"Debugger Not Found!", 0
-exp_pid 		dd 	0
-pid		 	dd 	0 
+exp_pid 	dd 	0
+pid		dd 	0 
 
 .data?
-hSnapshot   HANDLE 		       ?
-ProcEnt       PROCESSENTRY32 <?>
+hSnapshot   HANDLE  	    ?
+ProcEnt     PROCESSENTRY32 <?>
 
 .code
 start:
@@ -44,11 +44,11 @@ start:
         .IF (eax)
         @@:
            invoke lstrcmpi, ADDR exp , ADDR [ProcEnt.szExeFile]
-            .IF (eax == 0)
-                  lea ebx, [esi].th32ProcessID
-	         push [ebx]
-	         pop exp_pid
-	         jmp nextLoop
+            .IF (!eax)
+                lea ebx, [esi].th32ProcessID
+	        push [ebx]
+	        pop exp_pid
+	        jmp nextLoop
 	   .ELSE
             .ENDIF
             invoke Process32Next, hSnapshot, ADDR ProcEnt
@@ -69,19 +69,19 @@ nextLoop:
         invoke Process32First, hSnapshot, ADDR ProcEnt
         .IF (eax)
         @@:
-	         mov ebx, pid
-	         .IF ( ebx == [esi].th32ProcessID )
- 			mov ebx,  [exp_pid]       	
-         		.if ( ebx == [esi].th32ParentProcessID )
-         			  invoke MessageBox, NULL, ADDR dbgNotFound, ADDR AppName, MB_OK or MB_ICONINFORMATION
-         		.else
-         			 invoke MessageBox, NULL, ADDR dbgFound, ADDR AppName, MB_OK or MB_ICONERROR
-         		.endif
-	         .ELSE
-            	.ENDIF
-            invoke Process32Next, hSnapshot, ADDR ProcEnt
-            test eax,eax
-            jnz @B
+        mov ebx, pid
+        .IF ( ebx == [esi].th32ProcessID )
+ 		mov ebx,  [exp_pid]       	
+ 		.if ( ebx == [esi].th32ParentProcessID )
+ 		    invoke MessageBox, NULL, ADDR dbgNotFound, ADDR AppName, MB_OK or MB_ICONINFORMATION
+ 		.else
+ 		    invoke MessageBox, NULL, ADDR dbgFound, ADDR AppName, MB_OK or MB_ICONERROR
+ 		.endif
+	 .ELSE
+    	 .ENDIF
+	 invoke Process32Next, hSnapshot, ADDR ProcEnt
+	 test eax,eax
+	 jnz @B
         .ELSE
             invoke MessageBox, NULL, ADDR errProcFirst, ADDR AppName, MB_OK or MB_ICONERROR
         .ENDIF
